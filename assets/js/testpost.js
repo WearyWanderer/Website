b6dc6e9c506635b36bd863350df7ad90d79f6ac3
WearyWanderer <oglocisgangsta@hotmail.co.uk>
2016-08-01 21:04:31 +0100

    different js test

--- assets/js/testpost.js
+++ assets/js/testpost.js
@@ -1,10 +1,29 @@
-(function() {
-  window.onload = function() {
-    var dataset;
-    dataset = [5, 10, 15, 20, 25];
-    return d3.select("#example").selectAll("p").data(dataset).enter().append("p").text(function(d) {
-      return d;
-    });
-  };
+var data = [{year: 2006, books: 54},
+            {year: 2007, books: 43},
+            {year: 2008, books: 41},
+            {year: 2009, books: 44},
+            {year: 2010, books: 35}];
 
-}).call(this);
+var barWidth = 40;
+var width = (barWidth + 10) * data.length;
+var height = 200;
+
+var x = d3.scale.linear().domain([0, data.length]).range([0, width]);
+var y = d3.scale.linear().domain([0, d3.max(data, function(datum) { return datum.books; })]).
+  rangeRound([0, height]);
+
+// add the canvas to the DOM
+var barDemo = d3.select("#example").
+  append("svg:svg").
+  attr("width", width).
+  attr("height", height);
+
+barDemo.selectAll("rect").
+  data(data).
+  enter().
+  append("svg:rect").
+  attr("x", function(datum, index) { return x(index); }).
+  attr("y", function(datum) { return height - y(datum.books); }).
+  attr("height", function(datum) { return y(datum.books); }).
+  attr("width", barWidth).
+  attr("fill", "#2d578b");
