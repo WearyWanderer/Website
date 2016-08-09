6f493fd0caba3e4fe9e3482b7fb840ba01311605
WearyWanderer <oglocisgangsta@hotmail.co.uk>
2016-08-09 22:43:27 +0100

    Nice for 1920x1080 screen (resize)

--- assets/js/wanderlustmap.js
+++ assets/js/wanderlustmap.js
@@ -2,7 +2,7 @@ function initMap() {
 		var element = document.createElement('div');
         var mapDiv = document.getElementById('map-hider').appendChild(element);
 		mapDiv.id = 'map';
-		mapDiv.style.cssText = 'width:1000px; height:700px; margin: auto';
+		mapDiv.style.cssText = 'width:1400px; height:750px; margin: auto';
         var map = new google.maps.Map(mapDiv, {
             center: {lat: 44.540, lng: -78.546},
             zoom: 8
