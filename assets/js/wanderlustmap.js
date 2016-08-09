f66832d146709242cba0274f4894587b5dd287bd
WearyWanderer <oglocisgangsta@hotmail.co.uk>
2016-08-09 22:24:55 +0100

    Add css

--- assets/js/wanderlustmap.js
+++ assets/js/wanderlustmap.js
@@ -2,6 +2,7 @@ function initMap() {
 		
         document.getElementById('map-hider').appendChild("map");
 		var mapDiv = document.getElementById('map');
+		mapDiv.style.cssText = 'width:200px; height:200px;';
         var map = new google.maps.Map(mapDiv, {
             center: {lat: 44.540, lng: -78.546},
             zoom: 8
