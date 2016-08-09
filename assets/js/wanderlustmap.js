b31c26eea2d45c554f76dbdf0636bb5f8d5b959e
WearyWanderer <oglocisgangsta@hotmail.co.uk>
2016-08-09 22:38:08 +0100

    SOme bigger canvas and alginment

--- assets/js/wanderlustmap.js
+++ assets/js/wanderlustmap.js
@@ -2,7 +2,7 @@ function initMap() {
 		var element = document.createElement('div');
         var mapDiv = document.getElementById('map-hider').appendChild(element);
 		mapDiv.id = 'map';
-		mapDiv.style.cssText = 'width:200px; height:200px;';
+		mapDiv.style.cssText = 'width:1000px; height:700px; margin: auto';
         var map = new google.maps.Map(mapDiv, {
             center: {lat: 44.540, lng: -78.546},
             zoom: 8
